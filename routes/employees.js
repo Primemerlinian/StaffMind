cecee0aa6928e5ff9f8a104bc4af0e0932cd017e
Primemerlinian <mwatson0518@gmail.com>
2022-10-05 23:29:22 -0400

    add employee route

--- routes/employees.js
+++ routes/employees.js
@@ -1,2 +1,10 @@
 import { Router } from "express"
 const router = Router()
+import * as employeesCtrl from '../contollers/employees.js'
+
+router.get('/', employeesCtrl.index)
+router.get('/new', employeesCtrl.new)
+
+export {
+  router
+}
