e0ddcd7f6fb503d8f6b4e5d371d2800eb370d045
Primemerlinian <mwatson0518@gmail.com>
2022-10-05 23:24:38 -0400

    add employee model

---
+++ models/employee.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const employeeSchema  = new Schema ({ 
+  name: {
+    type: String, 
+    required: true, 
+  },
+  title: {
+    type: String,
+    enum: ['Team Lead', 'Account Manager', 'Engineer', 'Consultant', 'Associate']
+  },
+  hireDate: {
+    type: Date,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    min: 1, 
+    max: 999999,
+    required: true,
+  },
+  emergencyContact: {
+    type: String, 
+    required: true,
+  },
+  timestamps: true
+})
+const Employee = mongoose.model('Employee', employeeSchema)
+
+export {
+  Employee
+}
